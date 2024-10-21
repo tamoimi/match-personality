e759456c5c7ad6d7f81bee563135c03e353f69f7
tami <tamiortami@gmail.com>
2024-10-21 17:58:09 +0900

    refactor

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -8,16 +8,11 @@ interface Props {
 function Questions({ question, options, register, name }: Props) {
   return (
     <div className="mt-8">
-      <label className="font-semibold ">{question}</label>
+      <label className="font-semibold">{question}</label>
       <div className="flex gap-4">
         {options.map((option: any) => (
           <label key={option.value} className="p-2 text-sm border rounded-md cursor-pointer">
-            <input
-              type="radio"
-              value={option.value}
-              {...register(name, { required: true })} // register 직접 적용
-              className="mr-2"
-            />
+            <input type="radio" value={option.value} {...register(name, { required: true })} className="mr-2" />
             {option.label}
           </label>
         ))}
