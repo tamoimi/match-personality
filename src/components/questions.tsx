9a74f5cd6dc92da20fea8e3e8f2e19bfa6de4c5a
tami <tamiortami@gmail.com>
2024-10-23 11:53:44 +0900

    feat: types

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -1,6 +1,11 @@
+interface Option {
+  value: string;
+  label: string;
+}
+
 interface Props {
   question: string;
-  options: any;
+  options: Option[];
   register: any;
   name: string;
 }
@@ -10,7 +15,7 @@ function Questions({ question, options, register, name }: Props) {
     <div className="mt-8">
       <label className="font-semibold">{question}</label>
       <div className="flex gap-4">
-        {options.map((option: any) => (
+        {options.map((option: Option) => (
           <label key={option.value} className="p-2 text-sm border rounded-md cursor-pointer">
             <input type="radio" value={option.value} {...register(name, { required: true })} className="mr-2" />
             {option.label}
