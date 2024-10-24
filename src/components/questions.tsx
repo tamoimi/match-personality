5a2d2ad295c24b3d3ee647161c4e381a2b0e212c
tami <tamiortami@gmail.com>
2024-10-24 11:10:33 +0900

    style: flex-1

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -14,9 +14,9 @@ function Questions({ question, options, register, name }: Props) {
   return (
     <div className="mt-8">
       <label className="font-semibold">{question}</label>
-      <div className="flex gap-4">
+      <div className="flex gap-4 mt-3">
         {options.map((option: Option) => (
-          <label key={option.value} className="p-2 text-sm border rounded-md cursor-pointer">
+          <label key={option.value} className="flex-1 p-2 text-sm border rounded-md cursor-pointer">
             <input type="radio" value={option.value} {...register(name, { required: true })} className="mr-2" />
             {option.label}
           </label>
