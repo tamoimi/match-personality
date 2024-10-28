04e014110d39f89116877c2189501ae229658b99
tami <tamiortami@gmail.com>
2024-10-28 09:38:46 +0900

    style: bg-color

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -8,15 +8,23 @@ interface Props {
   options: Option[];
   register: any;
   name: string;
+  watch: any;
 }
 
-function Questions({ question, options, register, name }: Props) {
+function Questions({ question, options, register, name, watch }: Props) {
+  const selectedValue = watch(name);
+
   return (
     <div className="mt-8">
       <label className="font-semibold">{question}</label>
       <div className="flex gap-4 mt-3">
         {options.map((option: Option) => (
-          <label key={option.value} className="flex-1 p-2 text-sm border rounded-md cursor-pointer">
+          <label
+            key={option.value}
+            className={`flex-1 p-2 text-sm border rounded-md cursor-pointer ${
+              selectedValue === option.value ? "bg-gray-200" : ""
+            }`}
+          >
             <input type="radio" value={option.value} {...register(name, { required: true })} className="mr-2" />
             {option.label}
           </label>
