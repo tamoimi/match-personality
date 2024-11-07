9b8458835558e0279b72ead193069cd6c031c3b9
tami <tamiortami@gmail.com>
2024-11-07 10:34:11 +0900

    refactor: basic html tags

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -1,8 +1,3 @@
-import { useEffect } from "react";
-import { Label } from "./ui/label";
-import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
-import { Controller } from "react-hook-form";
-
 interface Option {
   value: string;
   label: string;
@@ -14,39 +9,32 @@ interface Props {
   register: any;
   name: string;
   watch: any;
-  control: any;
 }
 
-function Questions({ question, options, register, name, control }: Props) {
-
-  useEffect(() => {
-    // 처음 마운트될 때, 기본값이 있다면 설정
-    register(name, { required: true });
-  }, [name, register]);
+function Questions({ question, options, register, name, watch }: Props) {
+  const selectedValue = watch(name);
 
   return (
     <div className="mt-8">
       <label className="font-semibold">{question}</label>
-      <Controller
-        control={control}
-        name={name}
-        rules={{ required: true }}
-        render={({ field }) => (
-          <RadioGroup value={field.value || ""} onValueChange={field.onChange} className="flex gap-4 mt-3">
-            {options.map((option) => (
-              <div
-                key={option.value}
-                className={`flex-1 p-2 text-sm border rounded-md cursor-pointer items-center space-x-2 ${
-                  field.value === option.value ? "bg-gray-200" : ""
-                }`}
-              >
-                <RadioGroupItem value={option.value} id={option.value} />
-                <Label htmlFor={option.value}>{option.label}</Label>
-              </div>
-            ))}
-          </RadioGroup>
-        )}
-      />
+      <div className="flex gap-4 mt-3">
+        {options.map((option: Option) => (
+          <label
+            key={option.value}
+            className={`flex-1 p-2 text-sm border rounded-md cursor-pointer ${
+              selectedValue === option.value ? "bg-stone-100" : ""
+            }`}
+          >
+            <input
+              type="radio"
+              value={option.value}
+              {...register(name, { required: true })}
+              className="hidden mr-2 peer"
+            />
+            {option.label}
+          </label>
+        ))}
+      </div>
     </div>
   );
 }
